5987981a6c6578508cefea5affa035357719d30c
sylvaindurand <sylvaindurand@users.noreply.github.com>
2018-08-09 15:50:27 +0200

    Slideshow

---
+++ js/data.js
@@ -0,0 +1,10 @@
+var list = [
+    {
+        "img" : "puffins-hd.jpg",
+        "title" : "Puffins"
+    },
+    {
+        "img" : "godafoss_iceland.jpg",
+        "title" : "Goðafoss, Iceland"
+    }
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Clock
+
 function clock() {
     var time = new Date(),
         hours = time.getHours(),
@@ -9,3 +11,14 @@ function clock() {
 
 document.addEventListener('DOMContentLoaded', clock);
 setInterval(clock, 1000);
+
+
+// Slideshow
+
+function slideshow() {
+    var rand = list[Math.floor(Math.random() * list.length)];
+    document.body.style.backgroundImage = 'url("img/' + rand['img'] + '")';
+    document.querySelector('.location').textContent = rand['title'];
+}
+
+setInterval(slideshow, 3000);
