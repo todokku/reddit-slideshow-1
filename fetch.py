e6bc287c713b42fd73de2d4a9693b0fa4808cbf1
sylvaindurand <sylvaindurand@users.noreply.github.com>
2018-08-11 18:47:30 +0200

    Add variables and comments

--- fetch.py
+++ fetch.py
@@ -2,20 +2,29 @@ import urllib.request, json, re, os, pickle
 from PIL import Image
 from tqdm import tqdm
 
-subreddits = ["earthporn"]
-timeranges = ["all"]
-iterations = 10
+
+# Variables
+subreddits = ["earthporn"]  # which subreddits should we fetch?
+timeranges = ["all"]        # top images from: all, year, month, week, day?
+limit = 1000                # how many posts should we fetch?
+
+output_list = "js/data.js"
+output_path = "img/"
+
+min_width  = 1920           # minimal width, height and ratio to keep the image
+min_height = 1080
+min_ratio  =  1
 
 
-# Load existing files 
+# Load existing images list 
 try:
-    with open('js/data.js', 'r') as f:
+    with open(output_list, 'r') as f:
         imglist = f.read().replace('var list = ', '')
         imglist = json.loads(imglist)
 except:
     imglist = {}
 
-# Load blacklist 
+# Load existing blacklist
 try:
     with open ('blacklist', 'rb') as f:
         blacklist = pickle.load(f)
@@ -24,22 +33,19 @@ except:
 
 last = ""
 
-
+# Loop over subreddits and timeranges
 for subreddit in subreddits:
     for timerange in timeranges:
-        for i in range(iterations):
-            tqdm.write("ITERATION " + str(i))
-            url = "https://www.reddit.com/r/" + subreddit + "/top/.json?t=" + timerange + "&limit=100"
-            if last != "":
-                url += "&after=" + last
+        for i in tqdm(range(limit)):
 
+            # Reddit API to fetch posts
+            url = "https://www.reddit.com/r/" + subreddit + "/top/.json?t=" + timerange + "&limit=1" + "&after=" + last
             req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'})
 
             with urllib.request.urlopen(req) as url:
                 data = json.loads(url.read().decode())['data']['children']
 
-                for post in tqdm(data, leave=0):
-
+                for post in data:
                     # Name, url
                     name = post['data']['name']
                     url = post['data']['url']
@@ -57,24 +63,24 @@ for subreddit in subreddits:
                     title = re.sub(r'(\d)+ ?x ?(\d)+(.)*', '', title)
                     title = re.sub(r'(by )*@\w+', '', title)
                     title = re.sub(r'(,|\.) +$', '', title)
-                    tqdm.write(title)
+                    tqdm.write("\n" + title)
                     tqdm.write(url)
                     
                     # Download image
                     try:
-                        urllib.request.urlretrieve(url, "img/" + name + ".jpg")
+                        urllib.request.urlretrieve(url, output_path + name + ".jpg")
                 
                     except:
-                        blacklist.append(name)
+                        blacklist.append(name) # we won't try to download it again
                         continue
 
                     # Check image resolution and ratio
                     test = False
 
                     try:
-                        width, height = Image.open("img/" + name + ".jpg").size
-                        test = (width >= 1920 and height >= 1080 and width/height > 1)
-                        tqdm.write(str(width) + "x" + str(height) + ", ratio " + str(width/height) + " => " + str(test) + "\n")
+                        width, height = Image.open(output_path + name + ".jpg").size
+                        test = (width >= min_width and height >= min_height and width/height > min_ratio)
+                        tqdm.write(str(width) + "x" + str(height) + ", ratio " + str(width/height) + " => " + str(test))
 
                     except (KeyboardInterrupt, SystemExit):
                         raise
@@ -83,16 +89,16 @@ for subreddit in subreddits:
                         test = False
 
                     if (test == False):
-                        blacklist.append(name)
-                        os.remove("img/" + name + ".jpg")
+                        blacklist.append(name) # we won't try to download it again
+                        os.remove(output_path + name + ".jpg")
 
                     else:
                         imglist[name] = { 'title': title, 'file': name + ".jpg" }
 
-            # Export image list
-            with open('js/data.js', 'w') as f:
-                print('var list = ' + json.dumps(imglist), file=f)
+    # Export image list
+    with open('js/data.js', 'w') as f:
+        print('var list = ' + json.dumps(imglist), file=f)
 
-            # Save blacklist
-            with open('blacklist', 'wb') as f:
-                pickle.dump(blacklist, f)
+    # Export blacklist
+    with open('blacklist', 'wb') as f:
+        pickle.dump(blacklist, f)
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 // Clock
+// -----
 
 function clock() {
     var time = new Date(),
@@ -6,7 +7,7 @@ function clock() {
         minutes = time.getMinutes();
 
     document.querySelectorAll('.clock')[0].innerHTML = 
-        ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2);
+        ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2); // add a 0 before one-digit numbers
 }
 
 document.addEventListener('DOMContentLoaded', clock);
@@ -14,16 +15,17 @@ setInterval(clock, 1000);
 
 
 // Slideshow
+// ---------
 
 var keys = Object.keys(list);
-var rand = list[keys[ keys.length * Math.random() << 0]]; // First image
+var rand = list[keys[ keys.length * Math.random() << 0]];  // Select the first image randomly
 
 function slideshow() {
-    document.body.style.backgroundImage = 'url("img/' + rand['file'] + '")';
-    document.querySelector('.location').textContent = rand['title'];
+    document.body.style.backgroundImage = 'url("img/' + rand['file'] + '")'; // Update image
+    document.querySelector('.location').textContent = rand['title'];         // Update title
 
-    rand = list[keys[ keys.length * Math.random() << 0]];
-    var img=new Image(); // Preload next image
+    rand = list[keys[ keys.length * Math.random() << 0]];  // Select next image randomly
+    var img=new Image();                                   // Preload next image
     img.src = "img/" + rand['file'];
 }
 
